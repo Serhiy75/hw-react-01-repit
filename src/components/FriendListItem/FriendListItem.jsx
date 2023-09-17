91773c4406a287996f7ca18efbe597d83f9878a2
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-17 12:05:24 +0300

    hw-react-01 done

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,17 @@
 import css from './FriendListItem.module.css';
+import clsx from 'clsx';
 
 const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className="status">{isOnline ? 'Online' : 'Offline'}</span>
+      <span
+        className={clsx(css.base, {
+          [css.online]: isOnline,
+          [css.offline]: !isOnline,
+        })}
+      >
+        {isOnline ? 'Online' : 'Offline'}
+      </span>
       <img className="avatar" src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
